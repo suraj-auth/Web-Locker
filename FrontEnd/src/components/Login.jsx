8484a1f5af4e10e8fff95e8c9e2bb54819cc3d90
suraj-auth <surajmehto93@gmail.com>
2024-10-20 11:54:47 +0530

    Update Login.jsx

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -29,6 +29,7 @@ function Login() {
     else if (res.stat == "2") setError("username", { message: res.message });
     else if (res.stat == "3") setError("password", { message: res.message });
     else if (res.stat == "0") {
+      console.log("entered in the 0 condition");
       const days = 7;
       const date = new Date();
       date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
@@ -36,6 +37,9 @@ function Login() {
       document.cookie = "token=" + res.tkn + ";" + expires + ";path=/";
       navigate("/");
       alert("user navigated to /");
+    }
+    else{
+      console.log("entered in the else condition");
     }
   };
   return (
